9c068c5bf0db092285d6b0d4c82d9c58f982a6d3
DJan03 <danil2003nsk@gmail.com>
2020-12-20 11:59:14 +0300

    create empty classes Target, Ball, Cannon make main loop ready for changes

---
+++ cannonVsInvaders.py
@@ -0,0 +1,132 @@
+import pygame
+
+
+FPS = 60
+WIDTH, HEIGHT = 600, 400
+GRAVITY = 9.8
+RUNNING = True
+
+
+class Target:
+    shape_radius = 20
+    color = (255, 255, 0)
+    speed = 10
+
+    def __init__(self, x: int, y: int, speed):
+        self.x = x
+        self.y = y
+        self.speed = Target.speed
+        self.radius = Target.shape_radius
+        self.color = Target.color
+
+    def move(self):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
+class Ball:
+    shape_radius = 10
+    color = (255, 0, 0)
+
+    def __init__(self, x: int, y: int, velocity_x: float, velocity_y: float):
+        self.x = x
+        self.y = y
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+        self.shape_radius = Ball.shape_radius
+        self.color = Ball.color
+
+    def move(self):
+        pass
+
+    def collide(self, target: Target):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def bounce(self):
+        pass
+
+
+class Cannon:
+    color = (50, 50, 50)
+
+    def __init__(self, x: int, y: int, fire_power: float):
+        self.x = x
+        self.y = y
+        self.fire_power = fire_power
+        self.color = Cannon.color
+        self.aim_x = x
+        self.aim_y = y
+
+    def move(self, direction):
+        pass
+
+    def aim(self, position):
+        pass
+
+    def fire(self, balls: Ball):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
+def generate_targets(targets):
+    pass
+
+
+def input_handler(events, cannon: Cannon):
+    global RUNNING
+    for event in events:
+        if event.type == pygame.QUIT:
+            RUNNING = False
+
+
+def update_objects(cannon: Cannon, balls, targets):
+    for target in targets:
+        target.move()
+
+    for ball in balls:
+        ball.move()
+        for target in targets:
+            ball.collide(target)
+
+    cannon.move()
+
+
+def draw_objects(screen, cannon: Cannon, balls, targets):
+    for ball in balls:
+        ball.draw(screen)
+
+    for target in targets:
+        target.draw(screen)
+
+    cannon.draw(screen)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    cannon = Cannon(WIDTH // 2, HEIGHT * 9 // 10, 5)
+    balls = []
+    targets = []
+    generate_targets(targets)
+
+    while RUNNING:
+        input_handler(pygame.event.get(), cannon)
+
+        update_objects(cannon, balls, targets)
+        draw_objects(screen, cannon, balls, targets)
+
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
